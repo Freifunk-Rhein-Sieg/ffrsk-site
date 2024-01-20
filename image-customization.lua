4c731e7edf40bdae4c92bdc0dfc78acbe977087c
Claus Marxmeier <claus@marxmeier.de>
2024-01-20 13:08:21 +0100

    Update image-customization.lua

--- image-customization.lua
+++ image-customization.lua
@@ -1,14 +1,4 @@
-packages({'ff-mesh-vpn-tunneldigger',
-	'haveged' 
-    	'iwinfo',
-    	'iw-full', 
-    	'rsk-nightswitch',
-    	'gluon-rsk-block-mesh',
-    	'gluon-ping-check',
-    	'gluon-rsk-config',
-	'gluon-check-client-mesh',
-	'gluon-check-mesh'
-})
+-- Generic features and packages for all devices
 features({'autoupdater',
 	'banner',
 	'config-mode-autoupdater',
@@ -41,4 +31,76 @@ features({'autoupdater',
 	'web-wifi-config'
 })
 
+packages({'ff-mesh-vpn-tunneldigger',
+	'haveged' 
+    	'iwinfo',
+    	'iw-full', 
+    	'rsk-nightswitch',
+    	'gluon-rsk-block-mesh',
+    	'gluon-ping-check',
+    	'gluon-rsk-config',
+	'gluon-check-client-mesh',
+	'gluon-check-mesh'
+})
+
+-- Custom package lists
+local pkgs_usb_hid = {
+	'kmod-usb-hid',
+	'kmod-hid-generic'
+}
+
+local pkgs_usb_serial = {
+	'kmod-usb-serial',
+	'kmod-usb-serial-ch341',
+	'kmod-usb-serial-cp210x',
+	'kmod-usb-serial-ftdi',
+	'kmod-usb-serial-pl2303'
+}
+
+local pkgs_usb_storage = {
+	'block-mount',
+	'blkid',
+	'kmod-fs-ext4',
+	'kmod-fs-ntfs',
+	'kmod-fs-vfat',
+	'kmod-usb-storage',
+	'kmod-usb-storage-extras',  -- Card Readers
+	'kmod-usb-storage-uas',     -- USB Attached SCSI (UAS/UASP)
+	'kmod-nls-base',
+	'kmod-nls-cp1250',          -- NLS Codepage 1250 (Eastern Europe)
+	'kmod-nls-cp437',           -- NLS Codepage 437 (United States, Canada)
+	'kmod-nls-cp850',           -- NLS Codepage 850 (Europe)
+	'kmod-nls-cp852',           -- NLS Codepage 852 (Europe)
+	'kmod-nls-iso8859-1',       -- NLS ISO 8859-1 (Latin 1)
+	'kmod-nls-iso8859-13',      -- NLS ISO 8859-13 (Latin 7; Baltic)
+	'kmod-nls-iso8859-15',      -- NLS ISO 8859-15 (Latin 9)
+	'kmod-nls-iso8859-2',       -- NLS ISO 8859-2 (Latin 2)
+	'kmod-nls-utf8'             -- NLS UTF-8
+}
+
+local pkgs_usb_net = {
+	'kmod-mii',
+	'kmod-usb-net',
+	'kmod-usb-net-asix',
+	'kmod-usb-net-asix-ax88179',
+	'kmod-usb-net-cdc-eem',
+	'kmod-usb-net-cdc-ether',
+	'kmod-usb-net-cdc-subset',
+	'kmod-usb-net-dm9601-ether',
+	'kmod-usb-net-hso',
+	'kmod-usb-net-ipheth',
+	'kmod-usb-net-mcs7830',
+	'kmod-usb-net-pegasus',
+	'kmod-usb-net-rndis',
+	'kmod-usb-net-rtl8152',
+	'kmod-usb-net-smsc95xx',
+}
+
+local pkgs_pci = {
+	'pciutils'
+}
+
+local pkgs_pci_net = {
+	'kmod-bnx2'
+}
 
