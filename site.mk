c5053eb1d024a88d4b502b728cdd9a31f8f7c2a2
Claus Marxmeier <claus@marxmeier.de>
2025-07-08 22:47:29 +0200

    Update site.mk

--- site.mk
+++ site.mk
@@ -1,4 +1,4 @@
-DEFAULT_GLUON_RELEASE := stable-3.12.13
+DEFAULT_GLUON_RELEASE := stable-3.12.14
 # Allow overriding the release number from the command line
 
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
